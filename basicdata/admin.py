a10685c09d7a2d934ce3887e704831ee536ffeb7
HPetersen2 <petermann2@web.de>
2025-11-26 14:56:16 +0100

    add sellerchannel and promotioncode

--- basicdata/admin.py
+++ basicdata/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
-from .models import CustomerGroup, Brand, VehicleModel, VehicleType, PaydType
+from .models import CustomerGroup, Brand, VehicleModel, VehicleType, PaydType, SellerChannel, PromotionCode
 
 admin.site.register(CustomerGroup, ImportExportModelAdmin)
 admin.site.register(Brand, ImportExportModelAdmin)
 admin.site.register(VehicleModel, ImportExportModelAdmin)
 admin.site.register(VehicleType, ImportExportModelAdmin)
 admin.site.register(PaydType, ImportExportModelAdmin)
+admin.site.register(SellerChannel, ImportExportModelAdmin)
+admin.site.register(PromotionCode, ImportExportModelAdmin)
